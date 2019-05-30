f0597e68f43d1f35104819ac0a3c1f7d5ebcd2c6
ElectroStratz <electrostratz@gmail.com>
2019-05-30 12:33:09 +0100

    Read Video

--- app/src/main/java/com/example/movie_player/MainActivity.java
+++ app/src/main/java/com/example/movie_player/MainActivity.java
@@ -1,13 +1,115 @@
 package com.example.movie_player;
 
+
+import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+
+    //
+
+    ProgressDialog _dialog;
+
+    private Button _bbutton, _pbutton, _fbutton, _setUrlButton;
+    private VideoView _vView;
+    private SeekBar _sBar;
+
+    private EditText _URLtext;
+
+    //variables
+    private int _currentTime;
+    private ArrayList<String> _playlist;
+    private int _queuePos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _bbutton = findViewById(R.id.BackVid);
+        _pbutton = findViewById(R.id.PlayVid);
+        _fbutton = findViewById(R.id.FowardVid);
+
+        //test button
+        _setUrlButton = findViewById(R.id.button4);
+
+        _vView = findViewById(R.id.videoView);
+
+        _sBar = findViewById(R.id.seekBar);
+
+        _URLtext = findViewById(R.id.URL);
+
+        _currentTime = 0;
+
+        //check what does
+        //_vView.setMediaController(null);
+
+        //StartPlaylist();
+
     }
+
+    //sets up an empty playlist
+    private void StartPlaylist(){
+        _queuePos = 0;
+        _playlist = new ArrayList<>();
+        _playlist.add("https://img-9gag-fun.9cache.com/photo/aeMO8Zv_460svvp9.webm");
+        //_vView.setVideoPath(_playlist.get(_queuePos));
+    }
+
+    public void LoadVid(View v){
+        _dialog = new ProgressDialog(MainActivity.this);
+        _dialog.setMessage("Please wait...");
+        _dialog.setCanceledOnTouchOutside(false);
+        _dialog.show();
+
+        String _url = _URLtext.getText().toString();
+
+        try{
+            Uri _vUri = Uri.parse(_url);
+            _vView.setVideoURI(_vUri);
+            _vView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    _pbutton.setText("Pause");
+                }
+            });
+        }
+        catch (Exception ex){
+
+        }
+
+        _vView.requestFocus();
+        _vView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                _dialog.dismiss();
+                mp.setLooping(false);
+                _vView.start();
+            }
+        });
+    }
+
+    public void PlayVid(View v){
+
+        if(!_vView.isPlaying()){
+            _vView.start();
+            _pbutton.setText("Pause");
+        }
+        else{
+            _vView.pause();
+            _pbutton.setText("Play");
+        }
+
+    }
+
 }
